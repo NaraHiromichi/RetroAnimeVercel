ab5b6561340c49613f142c77c37b74e02b5520bc
NaraHiromichi <narahiromichi102938@gmail.com>
2023-08-09 16:12:43 +0630

    vercel version

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { store } from "./app/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Router basename="/Retroanime">
+    <Router basename="/">
       <DataProvider>
         <Routes>
           <Route path="/" element={<App />}>
